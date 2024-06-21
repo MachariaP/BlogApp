f01516914c30691cd1ee67cb96902f40bde51cd0
MachariaP <walburphinehas78@gmail.com>
2024-06-22 00:20:06 +0300

    Added publish, created and updated field to my post model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class POst(models.MOdel):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    creatwd = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
